888e5a3029b795a8c2f26bdd0a29a3a0f20797bb
adover134 <adfsfsf123@gmail.com>
2026-03-13 06:52:33 +0000

    레이아웃 수정

--- app.py
+++ app.py
@@ -12,22 +12,26 @@ st.title("MNIST 손글씨 숫자 예측하기")
 st.write("캔버스에 그린 숫자를 모델이 예측합니다.")
 
 # 캔버스 설정
-stroke_width = st.sidebar.slider("Stroke width: ", 1, 100, 5) # 선 굵기를 조정하는 slider입니다.
+stroke_width = 10 # 인식이 가장 잘 되는 선 굵기입니다.
 stroke_color = "#FFFFFF" # MNIST 데이터셋을 고려하여, 선은 흰색으로 고정하였습니다.
 bg_color = "#000" # 마찬가지 이유로 배경은 검은색으로 고정하였습니다.
 drawing_mode = "freedraw" # 손글씨라는 특징을 살리기 위해 freedraw로 지정하였습니다.
 
-canvas_result = st_canvas( # 캔버스 설정은 앞서 작성한 것들을 활용합니다.
-    stroke_width=stroke_width,
-    stroke_color=stroke_color,
-    background_color=bg_color,
-    height = 100, # 숫자 하나를 그리기에 충분한 공간을 줬습니다.
-    width = 100, # 정사각형임을 고려하여 높이와 같은 값을 줬습니다.
-    drawing_mode=drawing_mode,
-    key="canvas",
-)
+col1, col2 = st.columns(2)
 
-if canvas_result.image_data is not None:
+with col1:
+    st.title('숫자 그리기')
+    canvas_result = st_canvas( # 캔버스 설정은 앞서 작성한 것들을 활용합니다.
+        stroke_width=stroke_width,
+        stroke_color=stroke_color,
+        background_color=bg_color,
+        height = 100, # 숫자 하나를 그리기에 충분한 공간을 줬습니다.
+        width = 100, # 정사각형임을 고려하여 높이와 같은 값을 줬습니다.
+        drawing_mode=drawing_mode,
+        key="canvas",
+    )
+
+with col2:
     st.title('입력 이미지')
     st.text('모델이 실제로 입력받는 이미지로, 그린 것과 차이가 있을 수 있습니다.')
     transform = v2.Compose([
@@ -38,9 +42,15 @@ if canvas_result.image_data is not None:
     ])
     ti = transform(canvas_result.image_data)
     st.image(ti.permute(1, 2, 0).numpy(), clamp=True, channels='')
+
+if canvas_result.image_data is not None:
     res = predict_number(canvas_result.image_data)
-    prediction=pd.DataFrame.from_dict({'result':res, 'nums':list(range(10))})
+    prediction=pd.DataFrame.from_dict({'result':res})
     st.title('예측 결과')
     st.text('모델이 예측한 각 숫자 별 확률입니다.')
-    st.dataframe(prediction)
-    st.bar_chart(prediction, y='result', x='nums')
+
+    col3, col4 = st.columns(2)
+    with col3:
+        st.dataframe(prediction)
+    with col4:
+        st.bar_chart(prediction, y='result', horizontal=True)
